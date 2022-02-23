b723f9d027e8831005fa8de0aaae9fcf219106a2
FejzicF <fatimaahmedfejzic@gmail.com>
2022-02-23 21:34:10 +0100

    zadaca

---
+++ java zadaca 1/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Zovem se Fejzic Fatima");
+        System.out.println("Dolazim iz Gornje Tuzle <3");
+        System.out.println("Pohadam Behram-begovu medresu u Tuzli");
+        System.out.println("Volim sportove, prvenstveno borilacke sportove");
+        System.out.println("Zanima me psihologija i istrazujem neke slucajeve serijskih ubica zbog njihovih poremecaja");
+
+    }
+}
