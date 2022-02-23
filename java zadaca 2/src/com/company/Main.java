d4f7e754ba0c03da0f52ddcfbbc5705dadf30289
FejzicF <fatimaahmedfejzic@gmail.com>
2022-02-23 22:19:18 +0100

    zadaca1

---
+++ java zadaca 2/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double sideA = 5.1;
+        double area = 6 * sideA;
+        System.out.println("area");
+        System.out.println("volume");
+    }
+
+}
+
